49db4a24f0a731dd1618f19247e426859fe58318
Efemena <60428204+mvicm2k@users.noreply.github.com>
2020-07-28 19:35:55 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './App.css';
+import Footer from './components/Footer';
+import Products from './components/Products';
+
+function App() {
+  return (
+    <div className="App">
+      <Products />
+      <Footer/>
+    </div>
+  );
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+import * as serviceWorker from './serviceWorker';
+
+const client = new ApolloClient({
+  uri: "https://api.8base.com/ck7k8prgs000507lcg8pzef5r"
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
